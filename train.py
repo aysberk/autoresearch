103947b202e335c03afea4f9540faf85321435a6
aysberk1515 <aysberk1515@users.noreply.github.com>
2026-03-29 07:19:06 +0300

    deney: WINDOW=LL

--- train.py
+++ train.py
@@ -680,7 +680,7 @@ class MuonAdamW(torch.optim.Optimizer):
 
 ASPECT_RATIO = 64
 HEAD_DIM = 64
-WINDOW_PATTERN = "L"
+WINDOW_PATTERN = "LL"
 
 TOTAL_BATCH_SIZE = 2**16  # 64K tokens
 EMBEDDING_LR = 0.6
